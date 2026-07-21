e23d3c6ad25bd36ff712715225d6fd2130b19883
23-Fahad <ifahad2k@gmail.com>
2026-07-21 19:46:06 +0600

    fix(native-host): fix Jackson deserialization failure - add missing fields to StartMediaDownloadRequest and rebuild installDist
    The native host was crashing with UnrecognizedPropertyException when receiving
    START_MEDIA_DOWNLOAD messages from the browser extension because the
    StartMediaDownloadRequest record only had (url, formatId) but the extension
    sends (url, formatId, fileName). Added fileName/referer/userAgent fields and
    rebuilt the native host installDist JARs.

--- modules/browser-protocol/src/main/java/io/smartdm/browser/protocol/StartMediaDownloadRequest.java
+++ modules/browser-protocol/src/main/java/io/smartdm/browser/protocol/StartMediaDownloadRequest.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public record StartMediaDownloadRequest(
     @JsonProperty("url") String url,
     @JsonProperty("formatId") String formatId,
-    @JsonProperty("fileName") String fileName
+    @JsonProperty("fileName") String fileName,
+    @JsonProperty("referer") String referer,
+    @JsonProperty("userAgent") String userAgent
 ) implements NativeMessage {
 }
