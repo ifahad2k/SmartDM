fb9f0345f10121367c8f4f31583e3aaf5e412c8a
23-Fahad <ifahad2k@gmail.com>
2026-07-23 14:30:29 +0600

    fix(ytmusic): fix syntax error in playlist injection

--- extensions/chrome/ytmusic_overlay.js
+++ extensions/chrome/ytmusic_overlay.js
@@ -582,14 +582,14 @@
             fileName: null
         }, () => {
             setTimeout(() => {
-                btn.innerHTML = \`
+                btn.innerHTML = `
                     <svg class="icon" viewBox="0 0 24 24">
                       <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
                       <polyline points="7 10 12 15 17 10"></polyline>
                       <line x1="12" y1="15" x2="12" y2="3"></line>
                     </svg>
                     Download Playlist
-                \`;
+                `;
             }, 1000);
         });
     });
