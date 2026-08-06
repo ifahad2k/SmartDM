023101f1ea8637960af4fc249083db38c09344f1
23-Fahad <ifahad2k@gmail.com>
2026-08-06 19:58:28 +0600

    feat: Full SmartDM platform layout deployment with Header, Footer, and all sections

--- smartdm-website/src/main.tsx
+++ smartdm-website/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { HomePage } from './pages/HomePage';
+import { App } from './App';
 import './styles/globals.css';
 
 const rootEl = document.getElementById('root');
@@ -9,7 +9,7 @@ if (rootEl) {
   ReactDOM.createRoot(rootEl).render(
     <React.StrictMode>
       <BrowserRouter>
-        <HomePage />
+        <App />
       </BrowserRouter>
     </React.StrictMode>
   );
