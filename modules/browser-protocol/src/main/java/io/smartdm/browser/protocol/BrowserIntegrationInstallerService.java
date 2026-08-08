39c979772281934af9498a6d56c251c817adcc0f
23-Fahad <ifahad2k@gmail.com>
2026-08-08 19:08:13 +0600

    feat: register ExtensionInstallForcelist for Chrome, Edge, and Brave to auto-install extension on enterprise-managed profiles

--- modules/browser-protocol/src/main/java/io/smartdm/browser/protocol/BrowserIntegrationInstallerService.java
+++ modules/browser-protocol/src/main/java/io/smartdm/browser/protocol/BrowserIntegrationInstallerService.java
@@ -49,7 +49,7 @@ public class BrowserIntegrationInstallerService {
         Files.createDirectories(profileExtDir);
         copyDirectory(chromeExtDir, profileExtDir);
 
-        // 3. Register Browser Registry Extension path
+        // 3. Register Browser Registry Extension path & Policy
         String extRegPath = switch (profile.browserType().toLowerCase()) {
             case "edge" -> "HKCU\\Software\\Microsoft\\Edge\\Extensions\\knldjnnmkkebefogdbmggjijknmjeaoh";
             case "brave" -> "HKCU\\Software\\BraveSoftware\\Brave-Browser\\Extensions\\knldjnnmkkebefogdbmggjijknmjeaoh";
@@ -57,6 +57,13 @@ public class BrowserIntegrationInstallerService {
         };
         runCmd("reg", "add", extRegPath, "/v", "path", "/t", "REG_SZ", "/d", profileExtDir.toAbsolutePath().toString(), "/f");
         runCmd("reg", "add", extRegPath, "/v", "version", "/t", "REG_SZ", "/d", "1.0.6", "/f");
+
+        String forceRegPath = switch (profile.browserType().toLowerCase()) {
+            case "edge" -> "HKCU\\Software\\Policies\\Microsoft\\Edge\\ExtensionInstallForcelist";
+            case "brave" -> "HKCU\\Software\\Policies\\BraveSoftware\\Brave-Browser\\ExtensionInstallForcelist";
+            default -> "HKCU\\Software\\Policies\\Google\\Chrome\\ExtensionInstallForcelist";
+        };
+        runCmd("reg", "add", forceRegPath, "/v", "99", "/t", "REG_SZ", "/d", "knldjnnmkkebefogdbmggjijknmjeaoh;file:///" + profileExtDir.resolve("manifest.json").toAbsolutePath().toString().replace('\\', '/'), "/f");
 
         // 4. External Extension Directory Injection for Profile
         Path userData = profile.profilePath().getParent();
--- tools/scripts/build_single_exe_installer.ps1
+++ tools/scripts/build_single_exe_installer.ps1
@@ -102,6 +102,9 @@ reg add "HKCU\Software\Mozilla\NativeMessagingHosts\io.smartdm.host" /ve /t REG_
 reg add "HKCU\Software\Google\Chrome\Extensions\knldjnnmkkebefogdbmggjijknmjeaoh" /v "path" /t REG_SZ /d "%APP_DIR%extensions/chrome" /f >nul 2>&1
 reg add "HKCU\Software\Google\Chrome\Extensions\knldjnnmkkebefogdbmggjijknmjeaoh" /v "version" /t REG_SZ /d "1.0.6" /f >nul 2>&1
 
+reg add "HKCU\Software\Policies\Google\Chrome\ExtensionInstallForcelist" /v "99" /t REG_SZ /d "knldjnnmkkebefogdbmggjijknmjeaoh;file:///%APP_DIR%extensions/chrome/manifest.json" /f >nul 2>&1
+reg add "HKCU\Software\Policies\Microsoft\Edge\ExtensionInstallForcelist" /v "99" /t REG_SZ /d "knldjnnmkkebefogdbmggjijknmjeaoh;file:///%APP_DIR%extensions/chrome/manifest.json" /f >nul 2>&1
+
 reg add "HKCU\Software\Policies\Mozilla\Firefox\ExtensionSettings\smartdm-extension@smartdm.io" /v "installation_mode" /t REG_SZ /d "normal_installed" /f >nul 2>&1
 reg add "HKCU\Software\Policies\Mozilla\Firefox\ExtensionSettings\smartdm-extension@smartdm.io" /v "install_url" /t REG_SZ /d "file:///%APP_DIR%extensions/firefox/manifest.json" /f >nul 2>&1
 
